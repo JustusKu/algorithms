dc6c7d594fe5b3dd9f49e9bdbe49b56dcab2727d
JustusKu <asembopunga@gmail.com>
2025-11-03 23:32:13 +0300

    age programming

---
+++ script.js
@@ -0,0 +1,11 @@
+let userAge = prompt("Enter your age");
+console.log("Your age is " + userAge + " years old.");
+
+if (userAge >18){
+    console.log("Above 18");
+} else if(userAge <18){
+    console.log("Below 18");
+}else {
+    console.log("Enter valid integer")
+}
+
